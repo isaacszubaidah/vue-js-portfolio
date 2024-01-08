b25255f628b72b1f9c2d40d6fb6aa6535892eda0
isaacszubaidah <isaacszubaidah17@gmail.com>
2024-01-08 14:09:17 +0200

    add

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import EducationView from '../views/EducationView.vue'
+import WorkExperienceView from '../views/WorkExperienceView.vue'
+import SkillsView from '../views/SkillsView.vue'
+import ProjectsView from '../views/ProjectsView.vue'
+import ResumeView from '../views/ResumeView.vue'
+import ContactView from '../views/ContactView.vue'
 
 const routes = [
   {
@@ -18,6 +23,31 @@ const routes = [
     path: '/education',
     name: 'education',
     component: EducationView
+  },
+  {
+    path: '/workexperience',
+    name: 'workexperience',
+    component: WorkExperienceView
+  },
+  {
+    path: '/skills',
+    name: 'skills',
+    component:SkillsView
+  },
+  {
+    path: '/projects',
+    name: 'projects',
+    component: ProjectsView
+  },
+  {
+    path: '/resume',
+    name: 'resume',
+    component: ResumeView
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: ContactView
   },
 ]
 
