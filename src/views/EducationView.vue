e59e53efe4464c9599c41acd1f8d1cdf3e7154a3
isaacszubaidah <isaacszubaidah17@gmail.com>
2024-01-09 16:28:43 +0200

    added content

--- src/views/EducationView.vue
+++ src/views/EducationView.vue
@@ -1,8 +1,22 @@
   <template>
     <navbar/>
- <div class="education">
-<p>education page</p>
- </div>
+    <div class="vh-100">
+    <div class="education">
+      <div class="item-2">
+        <h1>Education</h1>
+        <div class="item-3">
+          <p>I matriculated in the year 2020.I was the valedictorian of my class that year.It was tough because of Covid and that is where I learnt that nothing can stop you, once you are committed and persevere.I won’t forget this quote from Hamlet , “To be or not to be” in the sense of is it nobler to drown in the sea of troubles or will you take arms and defeat it.</p>
+        </div>
+      </div>
+      <div class="item-1">
+        <img
+          class="EducationPageImg"
+          src="https://lh3.googleusercontent.com/drive-viewer/AEYmBYRmDEMj0-EdPhY6KA1atbD6nMcGODEzVF_NZCHOWZxIprioWPRnVRz15ViT7Gyk356l_nrv8cOyZH4HABLvUXE5Qx_2=s2560"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -16,6 +30,35 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.education {
+  margin-top: 5%;
+  display: grid;
+  grid: 700px / 1fr 1fr;
+}
+.EducationPageImg {
+  margin-top: 6%;
+  margin-right: 250px;
+  width: 320px;
+  height: 350px;
+}
+.item-2{
+  font-family: 'Playfair Display', serif;
+  margin-top:3%;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  color: #5E5343;
+}
+.item-3{
+  font-size:27px;
+  font-family: 'Playfair Display', serif;
+  margin-top: 8%;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  margin-left: 170px;
+  color: #5E5343;
+}
 
 </style>
