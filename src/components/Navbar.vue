b62e3aef32a7449c7e939b2fddda0d611a508cc1
isaacszubaidah <isaacszubaidah17@gmail.com>
2024-01-11 22:56:48 +0200

    wip

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,51 +1,96 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <router-link class="nav-link active" aria-current="page" to="/"><img class="logo" src="https://lh3.googleusercontent.com/drive-viewer/AEYmBYTtXe4LvlEcbgX8yA8-Wsnv3R-PqrE_Y4NizziZBcH8-5qjUQTdvIoo2PuF3LM5IY9UeDcRIV6iEUonR0JBQF3qdHPkUA=s2560" alt=""></router-link> 
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 center-nav">
-        <li class="nav-item">
-          <router-link class="nav-link active" aria-current="page" to="/">Home  |</router-link> 
-        </li> 
-        <li class="nav-item">
-              <router-link class="nav-link active" aria-current="page" to="/about">About  |</router-link> 
-        </li>
-        <li class="nav-item">
-              <router-link class="nav-link active" aria-current="page" to="/education">Education  |</router-link> 
-        </li>
-        <li class="nav-item">
-              <router-link class="nav-link active" aria-current="page" to="/resume">Resume  |
-              </router-link> 
-        </li>
-        <li class="nav-item">
-              <router-link class="nav-link active" aria-current="page" to="/projects">Projects  |</router-link> 
-        </li>
-        <li class="nav-item">
-              <router-link class="nav-link active" aria-current="page" to="/testimonials">Testimonials  |</router-link> 
-        </li>
-        <li class="nav-item">
-              <router-link class="nav-link active" aria-current="page" to="/contact">Contact </router-link> 
-        </li>
-      </ul>
+  <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+    <div class="container-fluid">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <router-link class="nav-link active" aria-current="page" to="/"
+          ><img
+            class="logo"
+            src="https://lh3.googleusercontent.com/drive-viewer/AEYmBYTtXe4LvlEcbgX8yA8-Wsnv3R-PqrE_Y4NizziZBcH8-5qjUQTdvIoo2PuF3LM5IY9UeDcRIV6iEUonR0JBQF3qdHPkUA=s2560"
+            alt=""
+        /></router-link>
+        <ul class="navbar-nav centerNav">
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/"
+              >Home |</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/about"
+              >About |</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              to="/education"
+              >Education |</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              to="/resume"
+              >Resume |
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              to="/projects"
+              >Projects |</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              to="/testimonials"
+              >Testimonials |</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              to="/contact"
+              >Contact
+            </router-link>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
-
+  </nav>
 </template>
 
 <script>
 export default {
-    name:"navbar",
-    props: {
-        msg: String,
-    },
-
-}
+  name: "navbar",
+  props: {
+    msg: String,
+  },
+};
 </script>
 
 <style scoped>
-
+.centerNav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: auto;
+  margin-left: auto;
+}
 </style>
