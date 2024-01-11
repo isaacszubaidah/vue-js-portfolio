641670332d5b7923ecd5591b907b7449286b9b5b
isaacszubaidah <isaacszubaidah17@gmail.com>
2024-01-11 23:21:23 +0200

    wip

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -52,7 +52,7 @@
           </label>
           <div>
             <button class="btn" type="submit" :disabled="isLoading">
-              <span v-if="isLoading">Loading...</span>
+              <span v-if="isLoading" class="loader"></span>
               <span v-else>Send</span>
             </button>
           </div>
@@ -148,6 +148,25 @@ export default {
 </script>
 
 <style scoped>
+.loader {
+  width: 20px;
+  height: 20px;
+  border: 2px solid black;
+  border-bottom-color: transparent;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 .formDiv {
   max-width: 200px;
   width: 100%;
